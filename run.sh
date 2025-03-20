9fbc91c3747cc511d8c035193b5c24eaaf385d1a
lucasparada20 <128637854+lucasparada20@users.noreply.github.com>
2025-03-20 16:05:31 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-build/exec_exact instance_file=instances_dins/Chicago_20_1.txt epsilon=0.2 delta=0.2 opt_cuts=6 instance_type=dins algorithm=dl
+build/exec_exact instance_file=instances_dins/Chicago_20_1.txt epsilon=0.2 delta=0.2 opt_cuts=2 instance_type=dins algorithm=dl
 
-#build/exec_exact instance_file=instances_pcg/ssbrp_30_20_0.txt epsilon=0.2 delta=0.2 opt_cuts=7  instance_type=pcg algorithm=dl
+#build/exec_exact instance_file=instances_pcg/ssbrp_30_20_0.txt epsilon=0.2 delta=0.2 opt_cuts=3  instance_type=pcg algorithm=dl
